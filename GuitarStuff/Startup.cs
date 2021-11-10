29c528b53e685002dbb3c7638a3db60ed85aa2ef
Alex Bertotto <ajb5206@gmail.com>
2021-11-09 19:19:07 -0800

    add swagger to Startup file with addswaggergen, useswagger, useswaggerUI and add swashbuckle to csproj
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

--- GuitarStuff/Startup.cs
+++ GuitarStuff/Startup.cs
@@ -24,6 +24,7 @@ namespace GuitarStuff
             services.AddDbContext<GuitarStuffContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +33,8 @@ namespace GuitarStuff
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             // app.UseHttpsRedirection();
